052d37bfd955e3ab9687491110fa64ca1343aad5
vitorchx <vitorchx@outlook.com>
2023-11-15 14:00:29 -0300

    test2

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int y = 32;
         double x = 10.35784;
+        Locale.setDefault(Locale.US);
         System.out.println(y);
         System.out.printf("%.2f%n", x);
         System.out.printf("%.4f%n", x);
         System.out.println("Bom dia!");
-        Locale.setDefault(Locale.US);
         System.out.printf("%.4f%n", x);
         System.out.println("Resultado = " + x + "metros");
         System.out.printf("Resultado = %.2f metros%n", x);
