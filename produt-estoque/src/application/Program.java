a548bda53744e9ad4747c0b2e38f220a7587bd36
vitorchx <vitorchx@outlook.com>
2024-01-02 20:38:07 -0300

    exec product in stock

---
+++ produt-estoque/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+
+        sc.close();
+    }
+}
