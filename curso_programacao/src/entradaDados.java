5f05a5ef131c51ff9aebebbff3200fa14187adcf
vitorchx <vitorchx@outlook.com>
2024-10-22 18:30:53 -0300

    entrada dados2

--- curso_programacao/src/entradaDados.java
+++ curso_programacao/src/entradaDados.java
@@ -5,12 +5,16 @@ public class entradaDados {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        char x;
-
-        x = sc.next() .charAt(0);
-
-        System.out.println("Voce digit: " + x);
-
+        String x;
+        int y;
+        double z;
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        System.out.println("Dados digitados: ");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
 
         sc.close();
     }
