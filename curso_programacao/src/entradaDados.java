91a7443b313c27eeebab3bb0d3468b754fba8bbc
vitorchx <vitorchx@outlook.com>
2024-10-22 18:45:03 -0300

    entrada de dados3

--- curso_programacao/src/entradaDados.java
+++ curso_programacao/src/entradaDados.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class entradaDados {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         String x;
         int y;
         double z;
@@ -16,6 +16,22 @@ public class entradaDados {
         System.out.println(y);
         System.out.println(z);
 
+        sc.close(); */
+
+        Scanner sc = new Scanner(System.in);
+        int x;
+        String s1, s2, s3;
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
         sc.close();
+
     }
 }
