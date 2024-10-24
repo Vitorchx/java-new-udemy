14874bc65fbf831c7deabad7667af7d42480c83c
vitorchx <vitorchx@outlook.com>
2024-10-24 17:17:10 -0300

    consicional simples

--- curso_programacao/src/inicioCurs/condicionalSimples.java
+++ curso_programacao/src/inicioCurs/condicionalSimples.java
@@ -12,15 +12,11 @@ public class condicionalSimples {
 
         if (hora < 12) {
             System.out.println("Bom dia!");
-        } else {
-            if (hora < 18)
-                System.out.println("Boa tarde!");
+        } else if (hora < 18)
+            System.out.println("Boa tarde!");
 
-            else {
-                System.out.println("Boa noite!");
-            }
-
+        else {
+            System.out.println("Boa noite!");
         }
-
     }
 }
