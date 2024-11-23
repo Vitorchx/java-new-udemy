f94ba6f092db57ea162e2f9c35d7b2a65a8efd14
vitorchx <vitorchx@outlook.com>
2024-11-23 12:21:20 -0300

    introdução entities triangulo

--- cursoPoo/src/application/program.java
+++ cursoPoo/src/application/program.java
@@ -6,6 +6,8 @@ possui a maior área.
 A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
 seguinte (fórmula de Heron) */
 
+import entities.triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,24 +16,31 @@ public class program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        double xA, xB, xC, yA, yB, yC;
+        triangle x, y;
+        x = new triangle();
+        y = new triangle();
 
         System.out.println("Enter the measures of triangle X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Enter the measures of triangle Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2.0;
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-        p = (yA + yB + yC) / 2.0;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
         System.out.printf("Triangle X area: %.4f%n", areaX);
+
         System.out.printf("Triangle Y area: %.4f%n", areaY);
+
         if (areaX > areaY) {
             System.out.println("Larger area: X");
         } else {
