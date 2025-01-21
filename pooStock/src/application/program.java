74a69ef2bb93142ddde6fb326c898ae476a18048
vitorchx <vitorchx@outlook.com>
2025-01-21 17:25:28 -0300

    omplementando metodos manualmente

--- pooStock/src/application/program.java
+++ pooStock/src/application/program.java
@@ -11,14 +11,21 @@ public class program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
+        Product p = new Product();
+
+
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+        Product product = new Product(name, price);
+
+        product.setName("Computer");
+        System.out.println("updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("updated price: " + product.getPrice());
+
         System.out.println();
         System.out.println("Product data: " + product);
         System.out.println();
