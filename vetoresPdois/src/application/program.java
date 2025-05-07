fde5961f21c01c394c8aead7a4f0f1918ce1838b
vitorchx <vitorchx@outlook.com>
2025-05-07 15:32:14 -0300

    medida valor de produtos no vetor

---
+++ vetoresPdois/src/application/program.java
@@ -0,0 +1,34 @@
+package application;
+
+import entities.product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        product[] vect = new product[n];
+
+        for (int i= 0; i<n; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price= sc.nextDouble();
+            vect[i]= new product(name, price);
+        }
+
+        double sum=0;
+        for (int i =0; i<n; i++){
+            sum += vect[i].getPrice();
+        }
+
+        double avg = sum/ n;
+        System.out.printf("Average price = %.2f%n", + avg);
+
+
+
+    }
+}
